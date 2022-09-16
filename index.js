51ca1993c76313a812e510ea5641f32128f42406
paomattjie <paolamattjie@hotmail.com>
2022-09-15 22:01:29 -0300

    crear usuario

---
+++ index.js
@@ -0,0 +1,20 @@
+const nombre='aldana'
+let edad=20
+const precio=120.0
+const seriesFav=['meteor garden','GOT','star wars']
+const peliculaFav={
+  'nombre':'Love, Rosie',
+  'anio':'2014',
+  'protagonistas':['Rosie','Alex','Katie']
+}
+
+console.log(nombre)
+console.log({nombre})
+console.log({edad})
+console.log({precio})
+console.log({seriesFav})
+console.log({peliculaFav})
+edad++
+console.log({edad})
+seriesFav.push('The Mandalorian')
+console.log({seriesFav})
